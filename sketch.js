de15ce3600bce3cc2073600d997db1edb3e880a2
NagaAkshay <69960504+NagaAkshay@users.noreply.github.com>
2020-09-08 12:56:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
+// Create the variables that are needed
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
 var packageBody,ground
+// Name Spacing
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
+// Load the Images that are needed
 function preload()
 {
 	helicopterIMG=loadImage("helicopter.png")
@@ -12,14 +15,20 @@ function preload()
 }
 
 function setup() {
+	
+	// Create the Canvas
 	createCanvas(800, 700);
+	
+	//Change the RectMode to Center
 	rectMode(CENTER);
 
+	// Add the PackagaeBody options
 	packageBody_options = {
 		restitution : 1.0
 	}
 	
 
+	// Create the Soprites that are needed
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
@@ -31,10 +40,11 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
-
+	// Create a physics Engine and store them in the variables
 	engine = Engine.create();
 	world = engine.world;
 
+	// Creatye the Packagebody using Bodies and add it to the world
 	packageBody = Bodies.circle(width/2 , 200 , 5 , packageBody_options);
 	World.add(world, packageBody);
 	
@@ -42,21 +52,27 @@ function setup() {
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
-
-
+	
+	// Run youe Physics Engine
 	Engine.run(engine);
   
 }
 
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
-  keyPressed();
-  drawSprites();
- 
+	// Change the RectMode tp Center
+	rectMode(CENTER);
+
+	// Make the Background BLACK
+	background(0);
+
+	// Position the PackagaeBody
+	packageSprite.x= packageBody.position.x 
+	packageSprite.y= packageBody.position.y 
+
+	// Draw the Sprites
+	drawSprites();
+
 }
 
 function keyPressed() {
